7d1560bb0822bda810744fe71d003494b1d8292e
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2021-03-15 10:00:38 +0100

    Add files via upload

---
+++ FeatureExtractor.py
@@ -0,0 +1,214 @@
+#! /usr/bin/python3
+
+import sys
+import argparse
+from os import listdir
+import string
+
+sys.path.append("../")
+
+import evaluator
+
+from xml.dom.minidom import parse
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+class FeatureExtractor():
+    def __init__(self):
+        args = self.parse_arguments()
+
+        self.datadir = "../"+ args["datadir"]
+        self.outfile_name = args["outfile"]
+        self.external = args["external"]
+
+        self.f = open(self.outfile_name, "w+")
+
+        self.stopwords = set(stopwords.words('english'))
+
+        print("Starting the process of directory " + self.datadir + " saved in " + self.outfile_name)
+        self.extract_dict()
+
+    def parse_arguments(self):
+        # construct the argument parser
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-datadir', '--datadir', type=str, default="data/train/", help='Directory with XML files to process')
+        parser.add_argument('-outfile', '--outfile', type=str, default="train.feat", help='Name for the output file')
+        parser.add_argument('--external', action="store_false", default=True, help='Whether to use external resources or not')
+
+        args = vars(parser.parse_args())
+        return args
+
+    def extract_dict(self):
+        if self.external:
+            # Loading DrugBank.txt
+            with open("../resources/DrugBank.txt", 'r', encoding='utf8') as doc:
+                document = doc.readlines()
+
+            self.drugbank_dict = {}
+            for d in document:
+                sep = d.rsplit('|', 1)
+                self.drugbank_dict[sep[0]] = sep[-1].rstrip()
+
+            # Loading HSDB.txt
+            with open("../resources/HSDB.txt", 'r', encoding='utf8') as doc:
+                document = doc.readlines()
+
+            self.HSDB = []
+            for d in document:
+                self.HSDB.append(d.rstrip().lower())
+
+    def tokenize(self, sentence):
+        '''
+        Task :
+        Given a sentence , calls nltk . tokenize to split it in
+        tokens , and adds to each token its start / end offset
+        in the original sentence .
+        Input :
+        s: string containing the text for one sentence
+        Output :
+        Returns a list of tuples (word , offsetFrom , offsetTo )
+        Example :
+        tokenize (" Ascorbic acid , aspirin , and the commoncold .")
+        [(" Ascorbic " ,0 ,7) , (" acid " ,9 ,12) , (" ," ,13 ,13) , ("
+        aspirin " ,15 ,21) , (" ," ,22 ,22) , (" and " ,24 ,26) , (" the
+        " ,28 ,30) , (" common " ,32 ,37) , (" cold " ,39 ,42) ,
+        ("." ,43 ,43) ]
+        '''
+        tokens = []
+        offset = 0
+
+        words = word_tokenize(sentence)
+
+        for w in words:
+            if (w in self.stopwords) or (w in string.punctuation):
+                continue
+            offset = sentence.find(w, offset)
+            tokens.append((w, offset, offset+len(w)-1))
+            offset+=len(w)+1
+
+        return tokens
+
+
+    def extract_features(self, s):
+        #TODO Add more conditions to extract better features
+        '''
+        Task :
+        Given a tokenized sentence , return a feature vector for each token
+        Input :
+        s: A tokenized sentence ( list of triples (word , offsetFrom , offsetTo ) )
+        Output :
+        A list of feature vectors , one per token .
+        Features are binary and vectors are in sparse representation (i.e. only active features are listed )
+        Example :
+        extract_features ([(" Ascorbic " ,0 ,7) , (" acid " ,9 ,12) , (" ," ,13 ,13) ,
+        (" aspirin " ,15 ,21) , (" ," ,22 ,22) , (" and " ,24 ,26) , (" the " ,28 ,30) ,
+        (" common " ,32 ,37) , (" cold " ,39 ,42) , ("." ,43 ,43) ])
+        [ [ " form = Ascorbic " , " suf4 = rbic ", " next = acid ", " prev = _BoS_ " , "capitalized" ],
+        [ " form = acid ", " suf4 = acid " , " next = ," , " prev = Ascorbic " ] ,
+        [ " form = ," , " suf4 = ," , " next = aspirin ", " prev = acid ", " punct " ],
+        [ " form = aspirin " , " suf4 = irin ", " next = ," , " prev = ," ],
+        ...
+        ]
+        '''
+        prev = "_BoS_"
+        feature_vectors = []
+        for i in range(len(s)):
+            if i == len(s)-1:
+                next = "_EoS_"
+            else:
+                next = s[i+1][0]
+            feature_vectors.append(["form = " + s[i][0], "suf4 = " + s[i][0][-4:], "pref4 = " + s[i][0][:4], "next = " + next, "prev = " + prev])
+            prev = s[i][0]
+            if s[i][0][0].isupper():
+                feature_vectors[i].append("capitalized")
+            if (s[i][0] in set(stopwords.words('english'))) or (s[i][0] in string.punctuation):
+                feature_vectors[i].append("punct")
+
+        return feature_vectors
+
+
+    def get_tag(self, token ,gold):
+        '''
+        Task :
+        Given a token and a list of ground truth entites in a sentence , decide
+        which is the B-I-O tag for the token
+        Input :
+        token : A token , i.e. one triple (word , offsetFrom , offsetTo )
+        gold : A list of ground truth entities , i.e. a list of triples (
+        offsetFrom , offsetTo , type )
+        Output :
+        The B-I-O ground truth tag for the given token ("B- drug ", "I- drug ", "Bgroup ", "I- group ", "O", ...)
+        Example :
+        get_tag ((" Ascorbic " ,0 ,7) , [(0 , 12 , " drug ") , (15 , 21 , " brand ") ])
+        B- drug
+        get_tag ((" acid " ,9 ,12) , [(0 , 12 , " drug ") , (15 , 21 , " brand ") ])
+        I- drug
+        get_tag ((" common " ,32 ,37) , [(0 , 12 , " drug ") , (15 , 21 , " brand ") ])
+        O
+        get_tag ((" aspirin " ,15 ,21) , [(0 , 12 , " drug ") , (15 , 21 , " brand ") ])
+        B- brand
+        '''
+
+        range1 = range(token[1], token[2])
+        for gold_word in gold:
+            range2 = range(gold_word[0], gold_word[1])
+            if range1.start in range2 and range1.stop-1 in range2:
+                if token[1] == gold_word[0]:
+                    return "B-" + gold_word[2]
+                else:
+                    return "I-" + gold_word[2]
+        return "O"
+
+    def test(self):
+        print(self.extract_features([("Ascorbic" ,0 ,7) , ("acid" ,9 ,12) , ("," ,13 ,13) ,
+        ("aspirin" ,15 ,21) , ("," ,22 ,22) , ("and" ,24 ,26) , ("the" ,28 ,30) ,
+        ("common" ,32 ,37) , ("cold" ,39 ,42) , ("." ,43 ,43) ]))
+        print("\n")
+        print(self.get_tag((" Ascorbic " ,0 ,7) , [(0 , 12 , "drug"), (15 , 21 , "brand")]))
+        print(self.get_tag((" acid " ,9 ,12) , [(0 , 12 , "drug"), (15 , 21 , "brand")]))
+        print(self.get_tag((" common " ,32 ,37) , [(0 , 12 , "drug"), (15 , 21 , "brand")]))
+        print(self.get_tag((" aspirin " ,15 ,21) , [(0 , 12 , "drug"), (15 , 21 , "brand")]))
+
+
+    def processFile(self):
+        # process each file in directory
+        for f in listdir(self.datadir):
+            # parse XML file , obtaining a DOM tree
+            tree = parse(self.datadir + "/" + f)
+            # process each sentence in the file
+            sentences = tree.getElementsByTagName("sentence")
+            for s in sentences:
+                sid = s.attributes["id"].value  # get sentence id
+                stext = s.attributes["text"].value  # get sentence text
+                # load ground truth entities .
+                gold = []
+                entities = s.getElementsByTagName("entity")
+                for e in entities:
+                    # for discontinuous entities , we only get the first span
+                    offset = e.attributes["charOffset"].value
+                    (start, end) = offset.split(";")[0].split("-")
+                    gold.append((int(start), int(end), e.attributes["type"].value))
+                # tokenize text
+                tokens = self.tokenize(stext)
+                # extract features for each word in the sentence
+                features = self.extract_features(tokens)
+
+                # print features in format suitable for the learner / classifier
+                for i in range(0, len(tokens)):
+                    # see if the token is part of an entity , and which part(B/I)
+                    tag = self.get_tag(tokens[i], gold)
+                    print(sid, tokens[i][0], tokens[i][1], tokens[i][2], tag, "\t".join(features[i]), sep='\t', file = self.f)
+                # blank line to separate sentences
+                print("", file = self.f)
+
+        self.f.close()
+
+
+if __name__ == '__main__':
+    featureExtractor = FeatureExtractor()
+    featureExtractor.processFile()
---
+++ Learner.py
@@ -0,0 +1,294 @@
+import sys
+import argparse
+from os import listdir
+import string
+import pandas as pd
+
+sys.path.append("../")
+
+import evaluator
+
+from itertools import chain
+import nltk
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
+import sklearn
+import pycrfsuite
+
+
+
+class Learner():
+    def __init__(self):
+        args = self.parse_arguments()
+
+        self.train_file = args["train"]
+        self.val_file = args["val"]
+        self.outfile_name = args["outfile"]
+        self.external = args["external"]
+        self.evaluator = args["evaluate"]
+
+        self.f = open(self.outfile_name, "w+")
+
+        print("Starting to train a learner saved in " + self.outfile_name)
+        self.extract_dict()
+
+    def parse_arguments(self):
+        # construct the argument parser
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-train', '--train', type=str, default="train.feat", help='Name for the training feature file')
+        parser.add_argument('-val', '--val', type=str, default="devel.feat", help='Name for the validation feature file')
+        parser.add_argument('-outfile', '--outfile', type=str, default="results.out", help='Name for the output file')
+        parser.add_argument('--external', action="store_false", default=True, help='Whether to use external resources or not')
+        parser.add_argument('-evaluate', '--evaluate', type=str, default="test", help='Evaluating over the testing dataset')
+
+
+        args = vars(parser.parse_args())
+        return args
+
+    def extract_dict(self):
+        if self.external:
+            # Loading DrugBank.txt
+            with open("../resources/DrugBank.txt", 'r', encoding='utf8') as doc:
+                document = doc.readlines()
+
+            self.drugbank_dict = {}
+            for d in document:
+                sep = d.rsplit('|', 1)
+                self.drugbank_dict[sep[0]] = sep[-1].rstrip()
+
+            # Loading HSDB.txt
+            with open("../resources/HSDB.txt", 'r', encoding='utf8') as doc:
+                document = doc.readlines()
+
+            self.HSDB = []
+            for d in document:
+                self.HSDB.append(d.rstrip().lower())
+
+    def sent2features(self, sent):
+        if len(sent)>3:
+            return [sent[5:]]
+        return []
+
+
+    def sent2labels(self, sent):
+        if len(sent)>3:
+            return [sent[4]]
+        return []
+
+
+    def sent2tokens(self, sent):
+        if len(sent)>3:
+            return [sent[:4]]
+        return []
+
+    # EVALUATE THE MODEL
+    def bio_classification_report(self, y_true, y_pred):
+        """
+        Classification report for a list of BIO-encoded sequences.
+        It computes token-level metrics and discards "O" labels.
+
+        Note that it requires scikit-learn 0.15+ (or a version from github master)
+        to calculate averages properly!
+        """
+        lb = LabelBinarizer()
+        y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+        y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+        tagset = set(lb.classes_) - {'O'}
+        tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+        print(tagset)
+        class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+        return classification_report(
+            y_true_combined,
+            y_pred_combined,
+            labels=[class_indices[cls] for cls in tagset],
+            target_names=tagset,
+        )
+
+    def print_transitions(self, trans_features):
+        for (label_from, label_to), weight in trans_features:
+            print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
+
+    def print_state_features(self, state_features):
+        for (attr, label), weight in state_features:
+            print("%0.6f %-6s %s" % (weight, label, attr))
+
+    def checkKnoweledge(self, tagger):
+        # let's check what classifier learned
+        from collections import Counter
+        info = tagger.info()
+
+        print("Top likely transitions:")
+        self.print_transitions(Counter(info.transitions).most_common(15))
+
+        print("\nTop unlikely transitions:")
+        self.print_transitions(Counter(info.transitions).most_common()[-15:])
+
+        print("Top positive:")
+        self.print_state_features(Counter(info.state_features).most_common(20))
+
+        print("\nTop negative:")
+        self.print_state_features(Counter(info.state_features).most_common()[-20:])
+
+    def readFile(self, filename):
+        with open(filename) as f:
+            content = f.readlines()
+        # you may also want to remove whitespace characters like `\n` at the end of each line
+        content = [x.strip().split("\t")for x in content]
+        return content
+
+    def createDataset(self):
+        self.train_sents = self.readFile(self.train_file)
+        self.test_sents = self.readFile(self.train_file)
+        self.X_train = [self.sent2features(s) for s in self.train_sents]
+        self.y_train = [self.sent2labels(s) for s in self.train_sents]
+
+        self.X_test = [self.sent2features(s) for s in self.test_sents]
+        self.y_test = [self.sent2labels(s) for s in self.test_sents]
+
+
+
+    def train(self):
+        self.createDataset()
+        trainer = pycrfsuite.Trainer(verbose=False)
+
+        for xseq, yseq in zip(self.X_train, self.y_train):
+            trainer.append(xseq, yseq)
+
+        trainer.set_params({
+            'c1': 0.2,  # coefficient for L1 penalty
+            'c2': 1e-3,  # coefficient for L2 penalty
+            # as we have a considerable quantity of features to train from
+            # the training should be longer
+            'max_iterations': 200,
+            # include transitions that are possible, but not observed
+            'feature.possible_transitions': True
+        })
+
+        trainer.train('pycrfsuite_10_03')
+
+    def classify(self):
+        # MAKE PREDICTIONS
+        tagger = pycrfsuite.Tagger()
+        tagger.open('pycrfsuite_10_03')
+        self.createDataset()
+        if self.evaluator is "test":
+            print("Evaluating the classifier over the testing dataset")
+            y_pred = [tagger.tag(xseq) for xseq in self.X_test]
+            #print(self.bio_classification_report(self.y_test, y_pred))
+            #self.checkKnoweledge(tagger)
+            self.output_results(y_pred)
+            self.f.close()
+            # print performance score
+            evaluator.evaluate("NER", "../data/test", self.outfile_name)
+        else:
+            print("Evaluating the classifier over the training dataset")
+            y_pred = [tagger.tag(xseq) for xseq in self.X_train]
+            #print(self.bio_classification_report(self.y_train, y_pred))
+            #self.checkKnoweledge(tagger)
+            self.output_results(y_pred)
+            self.f.close()
+            # print performance score
+            evaluator.evaluate("NER", "../data/train", self.f)
+
+    def output_results(self, y_pred):
+        self.tokens = [self.sent2tokens(s) for s in self.test_sents]
+        for index, pred in enumerate(y_pred):
+            if len(pred)>0:
+                tag = pred[0]
+                if 'O' not in tag:
+                    tag = tag.split("-")[1]
+                    e = self.createMap(self.tokens[index][0], tag)
+                    print(e["id"] + "|" + e["offset"] + "|" + e["name"] + "|" + e["type"], file=self.f)
+
+    def createMap(self, token, type):
+        return {'id': token[0], 'name': token[1], 'offset': str(token[2])+"-" +str(token[3]), 'type': type}
+
+    def output_entities (self, sid , tokens , tags ) :
+        '''
+        Task :
+        Given a list of tokens and the B-I-O tag for each token , produce a list
+        of drugs in the format expected by the evaluator .
+        Input :
+        sid : sentence identifier ( required by the evaluator output format )
+        tokens : List of tokens in the sentence , i.e. list of tuples (word ,
+        offsetFrom , offsetTo )
+        tags : List of B-I-O tags for each token
+        Output :
+        Prints to stdout the entities in the right format : one line per entity ,
+        fields separated by '|', field order : id , offset , name , type .
+        Example :
+        output_entities (" DDI - DrugBank . d553 .s0",
+        [(" Ascorbic " ,0 ,7) , (" acid " ,9 ,12) , (" ," ,13 ,13) ,
+        (" aspirin " ,15 ,21) , (" ," ,22 ,22) , (" and " ,24 ,26) ,
+        (" the " ,28 ,30) ,(" common " ,32 ,37) , (" cold " ,39 ,42) ],
+        ["B- drug ", "I- drug ", "O", "B- brand ", "O", "O", "O",
+        "O", "O "])
+        DDI - DrugBank . d553 .s0 |0 -12| Ascorbic acid | drug
+        DDI - DrugBank . d553 .s0 |15 -21| aspirin | brand
+        '''
+        # if it's not waiting will print the BI elements without the marks
+        # in order to not print the O's or print together the BI
+        wait = False  # while it's waiting will not print the elements
+        name = ''
+        off_start = '0'
+        element = {'name': '', 'offset': '', 'type': ''}
+        for ind, token in enumerate(tokens):
+            if tags[ind] == 'O':  # if it's a O element, we do nothing
+                wait = True
+            elif ind == len(tokens) - 1:  # if it's the last element of the sentence
+                if tags[ind].startswith('B'):
+                    element = {'name': token[0],
+                               'offset': str(token[1]) + '-' + str(token[2]),
+                               'type': tags[ind].split('-')[1]  # without B or I
+                               }
+                elif tags[ind].startswith('I'):
+                    element = {'name': name + ' ' + token[0],
+                               'offset': off_start + '-' + str(token[2]),
+                               'type': tags[ind].split('-')[1]
+                               }
+                    if name == '' and ind != len(tokens) - 1:
+                        element["name"] = token[0]
+                    wait = False
+                else:  # only to check
+                    print('There\'s something wrong')
+                wait = False
+
+            else:
+                if ((tags[ind].startswith('B') and tags[ind + 1].startswith('O')) or
+                        (tags[ind].startswith('B') and tags[ind + 1].startswith('B'))):
+                    element = {'name': token[0],
+                               'offset': str(token[1]) + '-' + str(token[2]),
+                               'type': tags[ind].split('-')[1]  # without B or I
+                               }
+                    wait = False
+                elif tags[ind].startswith('B') and tags[ind + 1].startswith('I'):
+                    name = token[0]
+                    off_start = str(token[1])
+                    wait = True
+                elif ((tags[ind].startswith('I') and tags[ind + 1].startswith('O')) or
+                      (tags[ind].startswith('I') and tags[ind + 1].startswith('B'))):
+                    element = {'name': name + ' ' + token[0],
+                               'offset': off_start + '-' + str(token[2]),
+                               'type': tags[ind].split('-')[1]
+                               }
+                    if name == '':
+                        element["name"] = token[0]
+                    wait = False
+                elif tags[ind].startswith('I') and tags[ind + 1].startswith('I'):
+                    if name == '':
+                        name = token[0]
+                    else:
+                        name = name + ' ' + token[0]
+                    wait = True
+                else:  # only to check
+                    print('There\'s something wrong2')
+
+            if not wait:
+                self.f.write(sid + '|' + element['offset'] + '|' + element['name'] + '|' + element['type'] + '\n')
+        self.f.close()
+
+if __name__ == '__main__':
+    learner = Learner()
+    #learner.train()
+    learner.classify()
