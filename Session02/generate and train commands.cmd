d88e540aafb695bbe804f07e80809e1083b0ea3b
juliogonzalez98 <63363749+juliogonzalez98@users.noreply.github.com>
2021-03-22 14:50:32 +0100

    Delete generate and train commands.cmd

--- Session02/generate and train commands.cmd
+++
@@ -1,5 +0,0 @@
-cd c:\Users\USER\Downloads\Julio\UPC\MATT\AHLT\AHLT-master\Session02
-C:/Users/USER/anaconda3/python.exe c:/Users/USER/Downloads/Julio/UPC/MATT/AHLT/AHLT/Session02/FeatureExtractor.py --datadir "data/train/" --outfile "train.feat"
-C:/Users/USER/anaconda3/python.exe c:/Users/USER/Downloads/Julio/UPC/MATT/AHLT/AHLT/Session02/FeatureExtractor.py --datadir "data/devel/" --outfile "devel.feat"
-C:/Users/USER/anaconda3/python.exe c:/Users/USER/Downloads/Julio/UPC/MATT/AHLT/AHLT/Session02/FeatureExtractor.py --datadir "data/test/" --outfile "test.feat"
-C:/Users/USER/anaconda3/python.exe c:/Users/USER/Downloads/Julio/UPC/MATT/AHLT/AHLT/Session02/Learner.py
