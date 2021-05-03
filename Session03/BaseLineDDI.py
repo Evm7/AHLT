68ece8482f97636ead9ccb08c316f4628a584bf5
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2021-05-03 16:18:08 +0200

    Great improvements, this version also allows to create json file to explore data

--- Session03/BaseLineDDI.py
+++ Session03/BaseLineDDI.py
@@ -34,6 +34,16 @@ class BaselineDDI():
         # connect to your CoreNLP server (just once)
         self.my_parser = CoreNLPDependencyParser(url="http://localhost:9000")
 
+        self.advise_pos = ["MD"]
+        self.interest_pos = ["NN", "VBZ", "VBN", "VBD", "VB", "MD", "RB", "VBP"]
+        self.advise_clues = ["should", "may", "could", "would"]
+        self.effect_clues = ["administered", "concurrently", "concomitantly", "increase", "increases", "increased", "effect",
+                        "effects", "prevent", "prevents", "prevented", "potentiate", "potentiates", "potentiated", "administer"]
+        self.mechanism_clues = ["inhibit", "reduce", "reduces", "reduced", "decrease", "decreases", "decreased", "change",
+                           "changes", "changed", "elevate", "elevates", "elevated", "interfere", "interferes",
+                           "interfered"]
+        self.int_clues = ["interaction", "intereact"]
+        self.id_parents_lemma = {}
 
         print("[INFO] Starting...", flush=True)
 
@@ -81,7 +91,7 @@ class BaselineDDI():
         mytree, = self.my_parser.raw_parse(s)
         i = 0
         offset = 0
-        for w in mytree.nodes:
+        for _ in mytree.nodes:
             word = str(mytree.get_by_address(i)["word"])
             offset = s.find(word, offset - 1)
             if offset == -1:
@@ -105,22 +115,141 @@ class BaselineDDI():
         ’, ’int ’) between e1 and e2 expressed by the sentence , or ’None ’ if no
         interaction is described .
         '''
-        word = "..."
-        if (int(e2[-1:]) - int(e1[-1:])) > 1:
-            for w in analysis.nodes:
-                if not ";" in entities.get(e1)[1] and not ";" in entities.get(e2)[0]:
-                    if int(analysis.get_by_address(w)["start"]) > int(entities.get(e1)[1]) and int(
-                            analysis.get_by_address(w)["start"]) < int(entities.get(e2)[0]):
-                        word = analysis.get_by_address(w)["word"]
-                    if word == "administer" or word == "potentiate" or word == "prevent":
-                        return "effect"
-                    elif word == "reduce" or word == "increase" or word == "decrease":
-                        return "mechanism"
-                    elif word == "interact" or word == "interaction":
-                        return "int"
-        return "null"
+        nodes = analysis.nodes
+        # Get entities
+        entity1, entity2 = self.getNodes(nodes, entities, e1, e2)
+        self.id_parents_lemma[self.id_pairs] = {}
+        self.id_parents_lemma[self.id_pairs]["error"]= False
+        # If entities can not be retrieved, no DDI
+        if len(entity1) < 1 or len(entity2)< 1:
+            self.id_parents_lemma[self.id_pairs]["error"] = True
+            return (0, "null")
+
+        # Get parent Nodes of each entity
+        parent1, rel1 = self.getParentNode(nodes, entity1)
+        parent2, rel2 = self.getParentNode(nodes, entity2)
+
+
+        self.id_parents_lemma[self.id_pairs]["parent1"] = parent1["lemma"]
+        self.id_parents_lemma[self.id_pairs]["parent2"] = parent2["lemma"]
+        self.id_parents_lemma[self.id_pairs]["is_under"] = False
+        self.id_parents_lemma[self.id_pairs]["tag"] = "None"
+
+        # If entity1 is under entity2, then no DDI
+        if self.is_under(entity1, entity2):
+            self.id_parents_lemma[self.id_pairs]["is_under"] = True
+            return (0, "null")
+
+        # Entities under same parent
+        if self.sameNode(parent1, parent2):
+            tag = parent1['tag'].lower()[0]
+            self.id_parents_lemma[self.id_pairs]["tag"] = tag
+
+            if tag not in ['v', 'n']:
+                return (0, "null")
+            if tag == 'v':
+                return (1, "advise")
+
+        self.responses1 = {}
+        self.responses1["effect"] = ['response', 'diminish', 'enhance']
+        self.responses1["mechanism"] = ['concentration', 'concentrations', 'absorption', 'metabolism', 'presence']
+        self.responses1["int"] =['interact', 'interaction']
+        self.responses1["advise"] = ['take', 'adjustment', 'avoid', 'recommend', 'contraindicate']
+
+        self.responses2 = {}
+        self.responses2["effect"] = ['effect']
+        self.responses2["mechanism"] = ['concentration','concentrations', 'absorption', 'metabolism', 'level', 'clearance']
+        self.responses2["advise"] = ['take', 'caution']
+
+        '''
+        self.responses1 = {}
+        self.responses1["effect"] = ['response', 'diminish', 'enhance']
+        self.responses1["mechanism"] = ['absorption', 'metabolism', 'presence']
+        self.responses1["int"] =['interact', 'interaction']
+        self.responses1["advise"] = ['take', 'adjustment', 'avoid', 'recommend', 'contraindicate']
+
+        self.responses2 = {}
+        self.responses2["effect"] = ['effect']
+        self.responses2["mechanism"] = ['absorption', 'metabolism', 'level', 'clearance']
+        self.responses2["advise"] = ['take', 'caution']'''
+
+
+
+        for classes_, lemmas in self.responses1.items():
+            if parent1["lemma"] in lemmas:
+                return (1, classes_)
+
+        for classes_, lemmas in self.responses2.items():
+            if parent2["lemma"] in lemmas:
+                return (1, classes_)
+        return (0, "null")
+
+
+    def check_inbetweens(self, start_e, end_e, start_k, end_k):
+        '''
+        Check whether an entity, defined by [start_e, end_e] is inside a given node [start_k, end_k]
+        in different ways: being entity in between node, or visceversa.
+        This way we are solving the issue with compounds nouns (group of nodes may define one same entity)
+        '''
+        ranger_tree = range(int(start_k), int(end_k) + 1)
+        ranger_entity = range(int(start_e), int(end_e) + 1)
+        return (int(start_e) in ranger_tree or int(end_e) in ranger_tree) or (start_k in ranger_entity and end_k in ranger_entity)
+
+    def getNodes(self, tree, entities, e1, e2):
+        """
+        Allows to return all the nodes which are referenced with the identifiers e1 and e1 given.
+        This method may allow to obtain a set of nodes which are owned by a given pair of entities.
+        """
+        ents1 = []
+        ents2 = []
+
+        start1 = entities[e1][0].split(";")
+        start2 = entities[e2][0].split(";")
+        end1 = entities[e1][1].split(";")
+        end2 = entities[e2][1].split(";")
+
+        for k in list(tree.keys()):
+            if 'start' in tree[k].keys():
+                for i in range(len(start1)):
+                    if self.check_inbetweens(start1[i], end1[i], tree[k]['start'], tree[k]['end']):
+                        ents1.append(tree[k])
+                for i in range(len(start2)):
+                    if self.check_inbetweens(start2[i], end2[i], tree[k]['start'], tree[k]['end']):
+                        ents2.append(tree[k])
+        return ents1, ents2
+
+    def getParentNode(self, tree, entity):
+        """
+        Function which obtains the parent of a given entity node and the type of relationship
+        """
+        if len(entity) == 1:
+            return tree[entity[0]['head']], entity[0]['rel']
+        parent = None
+        rel = None
+        for e in entity:
+            if e['head'] not in [other['address'] for other in entity]:
+                parent = tree[e['head']]
+                rel = e['rel']
+        return parent, rel
+
+    def is_under(self, entity1, entity2):
+        """
+        Check whether entity1 is under entity2 in the graph
+        """
+        for i in range(len(entity1)):
+            if entity1[i]['head'] in [e['address'] for e in entity2]:
+                return True
+        return False
+
+    def sameNode(self, node1, node2):
+        """
+        Check whether both nodes are in the same level/address
+        """
+        return node1['address'] == node2['address']
 
     def process_directory(self):
+        self.saver = {}
+        self.saver["DDI_Drugs"] =[]
         # process each file in directory
         length = len(listdir(self.datadir))
         for i, f in enumerate(listdir(self.datadir)):
@@ -131,6 +260,8 @@ class BaselineDDI():
             for s in sentences:
                 sid = s.attributes["id"].value  # get sentence id
                 stext = s.attributes["text"].value  # get sentence text
+                if len(stext) < 1:
+                    continue
                 # load sentence entities into a dictionary
                 entities = {}
                 ents = s.getElementsByTagName("entity")
@@ -139,8 +270,13 @@ class BaselineDDI():
                     entities[eid] = e.attributes["charOffset"].value.split("-")
 
                 # Tokenize , tag , and parse sentence
-                if "%" not in stext and len(stext) != 0:
+
+                if "%" not in stext:
                     analysis = self.analyze(stext)
+                else:
+                    stext = stext.replace("%", "p")
+                    analysis = self.analyze(stext)
+
 
                 # for each pair in the sentence , decide whether it is DDI and its type
                 pairs = s.getElementsByTagName("pair")
@@ -148,16 +284,32 @@ class BaselineDDI():
                     id_e1 = p.attributes["e1"].value
                     id_e2 = p.attributes["e2"].value
 
-                    ddi_type = self.check_interaction(analysis, entities, id_e1, id_e2)
-                    if ddi_type != None and ddi_type != "null":
+                    self.id_pairs = p.attributes["id"].value
+                    ddi = p.attributes['ddi'].value
+                    if ddi == "true":
+                        ddi_type = p.attributes['type'].value
+                        if ddi_type is None:
+                            ddi_type = 'null'
+
+                    find, ddi_type = self.check_interaction(analysis, entities, id_e1, id_e2)
+                    self.id_parents_lemma[self.id_pairs]["type"] = ddi_type
+
+                    if find:
                         print(sid + "|" + id_e1 + "|" + id_e2 + "|" + ddi_type, file = self.f)
-            if(i % (length/10) == 0):
-                print("[INFO] "+ (i/length)*100+" complete.")
+
+
+            if not (i % int(length/10)):
+                print("[INFO] "+ str(int(i/length*100))+"% complete.", flush=True)
 
         self.f.close()
+        import json
+        with open("clues.json", 'w') as outfile:
+            json.dump(self.id_parents_lemma, outfile)
+            outfile.close()
         # get performance score
         evaluator.evaluate("DDI", self.datadir, self.outfile_name)
 
 if __name__ == '__main__':
     baseline = BaselineDDI()
+    #print(baseline.analyze("However, halothane anesthetic requirement (i.e., MAC) was depressed in a dose-dependent fashion as much as 56% 1-2 hours and as much as 14% 5-6 hours after injection of ketamine, 50 mg/kg, im. "))
     baseline.process_directory()
